fe6b011dc7127ee17c1f0e1b64c7a28f164a85b0
LancelotHayward <hayward.lance@gmail.com>
2024-07-15 11:25:58 +0200

    EmployeeForm and updated home style

--- src/components/EmployeeForm/EmployeeForm.jsx
+++ src/components/EmployeeForm/EmployeeForm.jsx
@@ -1,9 +1,92 @@
 import "./EmployeeForm.scss"
 
+import LabeledInput from "../LabeledInput/LabeledInput.jsx"
+
+const states = [
+    "Alabama",
+    "Alaska",
+    "American Samoa",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "District Of Columbia",
+    "Federated States Of Micronesia",
+    "Florida",
+    "Georgia",
+    "Guam",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Marshall Islands",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Northern Mariana Islands",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Palau",
+    "Pennsylvania",
+    "Puerto Rico",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virgin Islands",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming"
+]
+
+function saveEmployee(e) {
+    e.preventDefault()
+}
+
 function EmployeeForm() {
     return (
         <form id="employeeform">
             <h2>Create Employee</h2>
+            <div>
+                <LabeledInput inputID="firstname" text="First Name"/>
+                <LabeledInput inputID="lastname" text="Last Name"/>
+                <LabeledInput inputID="birthdate" text="Date of Birth" type="date"/>
+                <LabeledInput inputID="startdate" text="Start Date" type="date"/>
+                <fieldset>
+                    <legend>Address</legend>
+                    <LabeledInput inputID="street"/>
+                    <LabeledInput inputID="city"/>
+                    <LabeledInput inputID="state" type="select" options={states}/>
+                    <LabeledInput inputID="zipcode" text="Zip Code" type="number"/>
+                </fieldset>
+                <LabeledInput inputID="department" type="select" options={["Sales", "Marketing", "Engineering", "Human Resources", "Legal"]}/>
+            </div>
+            <button onClick={saveEmployee}>Save</button>
         </form>
     )
 }
---
+++ src/components/LabeledInput/LabeledInput.jsx
@@ -0,0 +1,34 @@
+import "./LabeledInput.scss"
+
+import { useState } from 'react'
+
+function LabeledInput({inputID, text, type="text", options=[]}) {
+    const [input, setInput] = useState("")
+    // TODO : remove console log
+    if (input) {
+        console.log(input)
+    }
+    if (!text) {
+        text = inputID.charAt(0).toUpperCase() + inputID.slice(1)
+    }
+    function createInputOrSelect(type, inputID, options) {
+        if (type !== "select") {
+            return (
+                <input type={type} id={inputID} onChange={e => setInput(e.target.value)}/>
+            )
+        }
+        return (
+            <select name={inputID} id={inputID} onChange={e => setInput(e.target.value)}>
+                {options.map(option => (<option key={option}>{option}</option>))}
+            </select>
+        )
+    }
+    return (
+        <div className="labeledinput">
+            <label htmlFor={inputID}>{text}</label>
+            {createInputOrSelect(type, inputID, options)}
+        </div>
+    )
+}
+
+export default LabeledInput
