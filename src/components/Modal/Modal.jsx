e62bbc6b751f00d346f3dd94b7b03243115a5204
LancelotHayward <hayward.lance@gmail.com>
2024-07-17 10:02:47 +0200

    Improved styles and modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,9 @@
 import "./Modal.scss"
 
 function Modal({modalStatus=false, setModalStatus, text}) {
-    // Usually dialog's "open" doesn't have a value, but React currently omits attributes when their value is falsy.
+    if (!modalStatus) return
     return (
-        <dialog open={modalStatus} onClick={e => setModalStatus(false)} className="modal">
+        <dialog open onClick={e => setModalStatus(false)} className="modal">
             {text}
         </dialog>
     )
