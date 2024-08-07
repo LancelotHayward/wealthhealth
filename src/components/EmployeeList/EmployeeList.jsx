34d313eba65388f79dc53d9e73906e13a873268e
LancelotHayward <hayward.lance@gmail.com>
2024-08-07 16:59:25 +0200

    Improved react table

--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -1,28 +1,19 @@
 import { useMemo, useState, useEffect } from 'react'
 import { useSelector } from "react-redux"
-import { useTable } from "react-table";
-
-import ColumnHeader from "../ColumnHeader/ColumnHeader.jsx"
+import { useTable, useSortBy, useGlobalFilter } from "react-table";
 
 import "./EmployeeList.scss"
 
 function EmployeeList() {
     const [entriesCount, setEntriesCount] = useState(10)
-    const [search, setSearch] = useState("")
-    let storeData = useSelector((state) => state.list)
-    if (storeData.length === 0) {
-        storeData.push(["No data available in table"])
+    const storeData = useSelector((state) => state.list)
+    const [data, setData] = useState(storeData)
+    if (data.length > entriesCount) {
+      setData(storeData.slice(0, entriesCount))
     }
-    const entriesCountElement = (
-        <select id="entriesCount" onChange={e => setEntriesCount(e.target.value)}>
-            <option>10</option>
-            <option>25</option>
-            <option>50</option>
-            <option>100</option>
-        </select>
-    )
-    console.log(storeData)
-    const data = useMemo(() => storeData, []);
+    else if (data.length < entriesCount && storeData.length > data.length) {
+      setData(storeData.slice(0, entriesCount))
+    }
     const columns = useMemo(() => [
       {
         Header: "First Name",
@@ -61,9 +52,22 @@ function EmployeeList() {
         accessor: "zipcode",
       }
     ], [])
-
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data })
-    return (
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, state, setGlobalFilter } = useTable({ columns, data }, useGlobalFilter, useSortBy)
+    
+    if (storeData.length === 0) {
+        return (
+          <p>No employee data to show</p>
+        )
+    }
+    const entriesCountElement = (
+        <select id="entriesCount" onChange={e => setEntriesCount(e.target.value)}>
+            <option>10</option>
+            <option>25</option>
+            <option>50</option>
+            <option>100</option>
+        </select>
+    )
+      return (
         <div id="employeelist">
             <header>
                 <div>
@@ -71,20 +75,23 @@ function EmployeeList() {
                 </div>
                 <div>
                     <label htmlFor="search">Search:</label>
-                    <input type="text" id="search" onChange={e => setSearch(e.target.value)}/>
+                    <input type="text" id="search" onChange={e => setGlobalFilter(e.target.value)}/>
                 </div>
             </header>
             <table {...getTableProps()}>
                 <thead>
-                    {headerGroups.map((headerGroup) => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                                <th {...column.getHeaderProps()}>
-                                    {column.render("Header")}
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
+                  {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map((column) => (
+                        <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                            {column.render("Header")}
+                            <span>
+                              {column.isSorted ? (column.isSortedDesc ? " v" : " ^") : ""}
+                            </span>
+                        </th>
+                      ))}
+                    </tr>
+                  ))}
                 </thead>
                 <tbody {...getTableBodyProps()}>
                     {rows.map((row) => {
